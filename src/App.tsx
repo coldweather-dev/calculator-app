ed4e29de6efebef66ebb2ce2435b2b750b0a443a
ColdWeather <akshayejhajhra@gmail.com>
2023-01-17 18:04:19 +0530

    1. Creating project and setting up layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/* Calculator */}
+      <div className='Calculator-app'>
+
+        {/* Titlebar */}
+        <div className='Titlebar'>
+          App Title | Window Controls
+        </div>
+
+        <div className='Header'>
+          Calculations and Results
+        </div>
+
+        <div className='Body'>
+          Buttons
+        </div>
+
+      </div>
     </div>
   );
 }
