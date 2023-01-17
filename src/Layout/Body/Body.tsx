c854f77d972eebd5721237eadb067c209ab3ec80
ColdWeather <akshayejhajhra@gmail.com>
2023-01-17 22:00:58 +0530

    3. Adding all components

--- src/Layout/Body/Body.tsx
+++ src/Layout/Body/Body.tsx
@@ -1,10 +1,40 @@
 import React from "react";
+import "./Body.css";
 
 export default class Body extends React.Component {
     render() {
         return(
-            <div>
-                Body
+            <div className="Body">
+                <div className="Body-row">
+                    <button>AC</button>
+                    <button>AC</button>
+                    <button>%</button>
+                    <button>AC</button>
+                </div>
+                <div className="Body-row">
+                    <button>7</button>
+                    <button>8</button>
+                    <button>9</button>
+                    <button>x</button>
+                </div>
+                <div className="Body-row">
+                    <button>4</button>
+                    <button>5</button>
+                    <button>6</button>
+                    <button>-</button>
+                </div>
+                <div className="Body-row">
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button>+</button>
+                </div>
+                <div className="Body-row">
+                    <button>R</button>
+                    <button>0</button>
+                    <button>.</button>
+                    <button>=</button>
+                </div>
             </div>
         );
     }
--- src/Layout/Header/Header.tsx
+++ src/Layout/Header/Header.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import "./Header.css";
 
 export default class Header extends React.Component {
     render() {
         return(
-            <div>
-                Header
+            <div className="Header">
+                0
             </div>
         );
     }
